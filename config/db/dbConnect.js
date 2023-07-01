0435bfc953d6e5ea3f2722c14678a79bcfaee1c8
mukesh <mukeshmehta2041@gmail.com>
2023-07-01 23:14:04 +0530

    Added mongourl in dotenev

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -2,16 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://mukeshmehta2041:ekCNZgnKghBI2JlW@cluster0.ayzqnne.mongodb.net/myfirstdb"
-      //   {
-      //     useCreateIndex: true,
-      //     useFindAndModify: true,
-      //     useUnified: true,
-      //     useNewUrlParser: true,
-      //   }
-    );
-
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Db Connected successfully");
   } catch (error) {
     console.log(`Erroc ${error.message}`);

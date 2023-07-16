44be3f6cc3845091ab39350246a00eecdf532c63
mukesh <mukeshmehta2041@gmail.com>
2023-07-16 16:31:52 +0530

    Creating Post Model

---
+++ model/post/Post.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Post Title is required"],
+      trim: true,
+    },
+    //created by only category
+    category: {
+      type: String,
+      required: [true, "Post category is required"],
+      default: "All",
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisLiked: {
+      type: Boolean,
+      default: false,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Author is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Post description is required"],
+    },
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1682685796467-89a6f149f07a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+//compile into model
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;

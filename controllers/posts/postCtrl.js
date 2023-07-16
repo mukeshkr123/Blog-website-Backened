cfe2395ca4133311be14558a9ece8bfa2aecf649
mukesh <mukeshmehta2041@gmail.com>
2023-07-16 17:16:03 +0530

    Block user if post contains bad words

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -1,12 +1,27 @@
 const expressAsyncHandler = require("express-async-handler");
+const Filter = require("bad-words");
 const Post = require("../../model/post/Post");
 const validateMongoId = require("../../utils/validateMongodbID");
+const User = require("../../model/user/User");
 
+// Create post
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
-  const { user } = req.body;
+  const { _id } = req.user;
+  const { user, title, description } = req.body;
   validateMongoId(user);
+  // Check for bad words
+  const filter = new Filter();
+  const isProfane = filter.isProfane(title) || filter.isProfane(description);
+
+  if (isProfane) {
+    await User.findOneAndUpdate({ _id }, { isBlocked: true });
+    throw new Error(
+      "Creating Failed because it contains bad words and you have been blocked."
+    );
+  }
+
   try {
-    const post = await Post.create(req.body);
+    const post = await Post.create({ user, title, description });
     res.json(post);
   } catch (error) {
     res.json(error);
